1ef46e694fda4eccab9beeb3c82e32dfb09881c3
Bacho <bkart21@freeuni.edu.ge>
2023-09-10 01:50:37 -0700

    first commit

---
+++ mvp-project/mvp-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvp-project
+//
+//  Created by bacho kartsivadze on 10.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
